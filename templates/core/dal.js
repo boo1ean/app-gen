68302d0de6f748b3ca47064ac39fcfbf9b661c48
boo1ean <boo1ean0807@gmail.com>
2015-03-05 17:25:42 +0200

    Update dal generator

--- templates/core/dal.js
+++ templates/core/dal.js
@@ -2,8 +2,11 @@ var dal = require('../../framework/dal');
 var knex = require('../services/db');
 
 var table = '{{{ plural }}}';
+var fields = [{{{ _.pluck(columns, 'name').map(function (a) { return "'" + a + "'"; }).join(',') }}}];
+var softDeleteColumn = 'removed_at';
 
 module.exports = dal({
 	table: table,
-	knex: knex
+	knex: knex,
+	softDeleteColumn: softDeleteColumn
 });
