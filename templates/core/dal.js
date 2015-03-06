88b9c834b0c39ebf13a77ba702cf0b5dcf132bd3
boo1ean <boo1ean0807@gmail.com>
2015-03-06 10:35:20 +0200

    Tweak dal template, bump

--- templates/core/dal.js
+++ templates/core/dal.js
@@ -2,11 +2,12 @@ var dal = require('../../framework/dal');
 var knex = require('../services/db');
 
 var table = '{{{ plural }}}';
-var fields = [{{{ _.pluck(columns, 'name').map(function (a) { return "'" + a + "'"; }).join(',') }}}];
+var fields = [{{{ _.pluck(columns, 'name').map(function (a) { return "'" + a + "'"; }).join(', ') }}}];
 var softDeleteColumn = 'removed_at';
 
 module.exports = dal({
 	table: table,
 	knex: knex,
+	fields: fields,
 	softDeleteColumn: softDeleteColumn
 });
