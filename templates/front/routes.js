a2d04e557062b123796eef89155d6a08c55a2033
boo1ean <boo1ean0807@gmail.com>
2015-05-05 15:32:52 +0300

    Supplement generation pieces, bump

--- templates/front/routes.js
+++ templates/front/routes.js
@@ -1,2 +1,3 @@
 	$routeProvider.
+		when('/{{{ plural }}}/:id', { templateUrl: '{{{ plural }}}/{{{ singular }}}-edit.html', controller: '{{{ Singular }}}EditCtrl', reloadOnSearch: false }).
 		when('/{{{ plural }}}', { templateUrl: '{{{ plural }}}/{{{ plural }}}-list.html', controller: '{{{ Plural }}}ListCtrl', reloadOnSearch: false }).
