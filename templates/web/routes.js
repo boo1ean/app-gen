ba841dee11543229025ac842b9de301012bbd3a8
boo1ean <boo1ean0807@gmail.com>
2015-05-06 13:44:50 +0300

    Fix update route, bump

--- templates/web/routes.js
+++ templates/web/routes.js
@@ -3,6 +3,6 @@ var {{{ plural }}} = controller(require('./controllers/{{{ plural }}}'));
 module.exports = function configureRoutes (app) {
 	app.get('/api/{{{ plural }}}', {{{ plural }}}.query);
 	app.post('/api/{{{ plural }}}', {{{ plural }}}.create);
-	app.put('/api/{{{ plural }}}', {{{ plural }}}.update);
+	app.put('/api/{{{ plural }}}/:id', {{{ plural }}}.update);
 	app.get('/api/{{{ plural }}}/:id', {{{ plural }}}.find);
 	app.delete('/api/{{{ plural }}}/:id', {{{ plural }}}.remove);
